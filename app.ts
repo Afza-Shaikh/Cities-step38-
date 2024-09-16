eb9bb80a082b9c53a114353a18e11c035053fabd
afza shaikh <shaikhafza961@gmail.com>
2024-09-16 22:50:13 +0500

    Add files via upload

---
+++ app.ts
@@ -0,0 +1,10 @@
+let karachi = "karachi is in pakistan"
+let paris = "paris is in france"
+let barcelona = "barcelona is in spain"
+
+function call_city(city: string){
+    console.log(city)
+}
+console.log(karachi)
+console.log(paris)
+console.log(barcelona)
